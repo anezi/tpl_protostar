c4aae71655126d2799f63061c256e8fddfae2fb5
Hassan Amouhzi <github@anezi.net>
2013-08-06 20:39:57 +0200

    update

--- deploy.php
+++ deploy.php
@@ -2,8 +2,4 @@
 
 echo '<br/>' . PHP_EOL;
 echo shell_exec('git pull');
-echo '<br/>' . PHP_EOL;
-echo shell_exec('git clean -f -d');
-echo '<br/>' . PHP_EOL;
-echo shell_exec('git pull');
 
--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 git add *
 git commit -m $1
-git push -f
+git push
 
